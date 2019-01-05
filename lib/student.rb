5283014619acb5974550c557494d0587786b1da9
Sean Langton <slangton1@gmail.com>
2019-01-05 06:17:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,9 @@ class Student
       FROM students
       WHERE grade < 12
       SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def save
