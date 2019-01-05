f4fe83bc2f218f86ae9b73bf304b414102840992
Sean Langton <slangton1@gmail.com>
2019-01-05 06:11:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,8 +27,15 @@ class Student
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
+  end
 
-
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = "9th"
+      SQL
+    DB[:conn].execute(sql)
   end
 
   def save
